8a5ec50dbb5b7b13fd32efb3f7ba175323a0a76f
SouravPaul2002 <sourav97972@gmail.com>
2025-08-10 23:35:21 +0530

    update

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -7,7 +7,7 @@ const { headers } = require("next/headers")
 
 const API_KEY=process.env.NEXT_PUBLIC_STRAPI_API_KEY
 const axiosClient = axios.create({
-    baseURL:'http://localhost:1337/api',
+    baseURL:'https://restaurant-booking-app-strapi.onrender.com/api',
     headers:{
         'Authorization': `Bearer ${API_KEY}`
     }
